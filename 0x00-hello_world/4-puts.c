215c64c8648e68f3490a611b19f7c6352747b08a
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-16 13:59:18 -0700

    first c program in alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *main - this is the main funcyion
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	/*printing this statment*/
 	puts("\"Programming is like building a multilingual puzzle");
-
-
-
 	return (0);
 }
