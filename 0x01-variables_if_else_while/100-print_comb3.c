0fa8931696189210ad13c1165cc1bd13a4457bb2
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-17 14:18:15 -0700

    nested for

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print nombers again
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			if (j > 0)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 56 && j == 57)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
