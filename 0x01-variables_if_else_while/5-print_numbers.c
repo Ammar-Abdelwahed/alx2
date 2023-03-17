3abe9bb015163b874f24b18883ba01a07f0ec5c8
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-17 13:30:51 -0700

    print for 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print from 0 to 9
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
