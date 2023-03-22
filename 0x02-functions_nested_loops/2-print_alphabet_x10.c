2c3d4b3ae499f561709feeb3be43c7c28f899e58
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-21 17:54:01 -0700

    SAs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet_x10 print the alphabe 10 times
+ * print_alphabet_x10 - print alphabe ten times
  */
-void  print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	int j;
 
-	for (j = 0; j < 11; j++)
+	for(i = 0; i <=10; i++)
 	{
-		for(i = 97; i <= 122; i++)
+		for(j = 97; j <= 122; j++)
 		{
-			_putchar(i);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
