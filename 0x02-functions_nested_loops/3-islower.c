ba593b01bfb538174cdab74509c57447c3ca1eb2
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-21 18:12:59 -0700

    sa

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check if the letter is in lower or upper case
+ * @c: is the letter
+ * Return: 1 if the letter is lower. 0 if the letter is upper
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
