5d43af5f9755f2252f5cf94c59f92516b9d80760
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-24 17:57:25 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - print a straight line
+ * @n: length of the straiht line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
