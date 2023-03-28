9317662a16e127e71a09915b3aa689571d094ccd
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-03-27 18:12:46 -0700

    reversing a string using pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,25 +6,19 @@
 void rev_string(char *s)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 	char c;
 	/* get the last character of the string */
 	while (s[i] != 0)
 	{
 		i++;
 	}
-	/* decrease the value by 1 because of the null value */
-	if (i > 0)
+
+	for (j = 0; j < i; j++)
 	{
 		i--;
-	}
-
-	while (j != i)
-	{
 		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
-		j++;
-		i--;
 	}
 }
