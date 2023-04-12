f93bea7e71bd2017f9573b29cbdd471cb914299c
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-04-11 19:20:38 -0700

    making two dimensin array using malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(int) * height);
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 	{
 		return (NULL);
